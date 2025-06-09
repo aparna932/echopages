bed25debe84164ebd1b66112b2c82546fec225d9
aparna932 <aparnika24@gmail.com>
2025-06-09 10:38:33 +0530

    Delete backend directory

--- backend/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchDiaries } = require("../controllers/searchController");
-
-router.get("/", searchDiaries);
-
-module.exports = router;
